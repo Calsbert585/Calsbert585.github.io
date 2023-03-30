347fd9522ec5d9055cf92f5cb1f44a8eb652ef5b
Calbert585 <aaaaaaaawesome2010@gmail.com>
2023-03-30 07:58:22 -0400

    Update and rename unknown to blog.js

---
+++ blog.js
@@ -0,0 +1,6 @@
+import "./styles.css";
+
+document.getElementById("app").innerHTML = `
+<h1>Hello world!</h1>
+<div>
+  This is my blog, i guess, it will be very simple because i don't know how to code but i hope it will entertain at least someone
